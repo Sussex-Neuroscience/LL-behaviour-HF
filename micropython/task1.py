64a0b89b4cc8adc56b3f998e62ca14b0aec785be
amchagas <andremaia.chagas@gmail.com>
2021-04-08 12:15:35 +0100

    first pass at task1

---
+++ micropython/task1.py
@@ -0,0 +1,18 @@
+
+
+class task1:
+    def __init__(self):
+        self.iti = 5000 #inter trial interval in ms
+        self.baseline = 10000 # time to wait at the beginning of session to record baseline
+        self.stimDuration = 5000 # stimulus presentation duration
+        self.responseWindowDuration = 2000 #time window to respond after stim presentation
+        self.rewardDuration = 1000 # duration the solenoid valves will stay in open state, which ends up being the amount of water offered
+        self.numberOfTrials = 100 #the number of trials that will be presented to the animals
+        self.trial = 1 #the current trial
+
+
+    def task():
+        while self.trial<=self.numberOfTrials:
+            #do things
+            
+    
